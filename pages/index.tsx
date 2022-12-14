5d62cb55c545a26c1d57c6cb03fd7ddb8b6c8d66
jynam <njy1007@gmail.com>
2022-12-14 21:26:42 +0900

    feat: Session 퍼블리싱

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import styled from '@emotion/styled';
 import Information from '../components/Information';
+import Session from '../components/Session';
 
 export default function Home() {
   return (
     <div>
       <TempContainer>gif 영역</TempContainer>
       <Information />
+      <Session />
     </div>
   );
 }
