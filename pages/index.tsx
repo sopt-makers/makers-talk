9761b096cb7b6a64a5dc4333f96e14aae152eb5d
jynam <njy1007@gmail.com>
2022-12-14 21:37:04 +0900

    feat: SpeakerIntroduction 퍼블리싱

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import styled from '@emotion/styled';
 import Information from '../components/Information';
 import Session from '../components/Session';
+import SpeakerIntroduction from '../components/SpeakerIntroduction';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <TempContainer>gif 영역</TempContainer>
       <Information />
       <Session />
-    </div>
+      <SpeakerIntroduction />
+    </>
   );
 }
 
