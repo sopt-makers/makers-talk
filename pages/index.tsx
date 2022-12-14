ea37f0b97a0fc5ef4dac7dfedaf0ced56bd0e514
jynam <njy1007@gmail.com>
2022-12-14 21:03:13 +0900

    feat: Infomation 퍼블리싱

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
+import styled from '@emotion/styled';
+import Information from '../components/Information';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <TempContainer>gif 영역</TempContainer>
+      <Information />
+    </div>
+  );
 }
+
+const TempContainer = styled.div`
+  height: 618px;
+  background-color: royalblue;
+`;
