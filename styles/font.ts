e2805eac36ecf93f82ad557cc9aba4b5d1fa76ab
jynam <njy1007@gmail.com>
2022-12-14 19:54:35 +0900

    chore: 글로벌 스타일 설정

---
+++ styles/font.ts
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react';
+
+const font = css`
+  @font-face {
+    font-family: 'SUIT-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+`;
+
+export default font;
